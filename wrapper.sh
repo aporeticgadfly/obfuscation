1d8a444142bd1f0ccae9ea191feb822b3455a11b
aptaabye <santiorellana@live.ca>
2020-03-27 01:25:37 -0400

    initial commit

---
+++ wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -
+#
+# Desc: 
+# executing an encrypted wrapped script
+#
+# Usage:
+# wrapper.sh
+#    Enter the password when prompted
+#
+
+encrypted='U2FsdGVkX18WvDOyPFcvyvAozJHS3tjrZIPlZM9xRhz0tuwzDrKhKBBuugLxzp7T
+MoJoqx02tX7KLhATS0Vqgze1C+kzFxtKyDAh9Nm2N0HXfSNuo9YfYD+15DoXEGPd'   #<1>
+
+read -s word    #<2>
+
+innerScript=$(echo "$encrypted" | openssl aes-256-cbc -base64 -d -pass pass:"$word")   #<3>
+
+eval "$innerScript"   #<4>
